3d0e6549fae6cc06ba3d3afbecf88ac3b5d061a2
Louis Charnay <louis.charnay@orange.fr>
2022-01-30 13:40:54 +0100

    Tp1ex4

--- src/Controller/PicturesController.php
+++ src/Controller/PicturesController.php
@@ -5,24 +5,35 @@ namespace App\Controller;
 class PicturesController extends AppController {
     public function index() {
         $pictures = glob('img/*.jpg');
-        $json = json_encode($pictures, JSON_PRETTY_PRINT);
+        $json = json_encode($pictures);
         print_r($json);
         $response = $this->response->withStringBody($json);
         return $response;
     }
 
     public function view() {
-        $pictures = glob('img/*.jpg');
+        $pictures = glob('img\*.jpg');
+        $limit = $_GET['limit']??-1;
+        $name = $_GET['name']??-1;
+        $cpt = 0;
         foreach($pictures as $image) {
-            $exif[$image]['description'] = exif_read_data($image)['ImageDescription']??'No descritpion';
-            $exif[$image]['comment'] = exif_read_data($image)['Comment']??'No comment';
-            $exif[$image]['author'] = exif_read_data($image)['Artist']??'No author';
-            $exif[$image]['width'] = exif_read_data($image)['width']??'No width';
-            $exif[$image]['height'] = exif_read_data($image)['height']??'No height';
-            $exif[$image]['html'] = '<img src=\"' . $image . '\" alt=\"' . $exif[$image]['comment'] . '\">';
+            if ($cpt < $limit || $limit == -1) {
+                $exif[$image]['description'] = exif_read_data($image)['ImageDescription']??'No descritpion';
+                $exif[$image]['comment'] = exif_read_data($image)['COMPUTED']['UserComment']??'No Comment';
+                $exif[$image]['author'] = exif_read_data($image)['Artist']??'No author';
+                $exif[$image]['width'] = exif_read_data($image)['COMPUTED']['Width']??'No width';
+                $exif[$image]['height'] = exif_read_data($image)['COMPUTED']['Height']??'No height';
+                $exif[$image]['html'] = '<img src=..\\' . $image . ' alt=' . $exif[$image]['comment'] . '>';
+                $cpt++;
+            }
         }
-        $json = json_encode($exif, JSON_PRETTY_PRINT);
+        if ($name == -1){
+            $json = json_encode($exif);
+        }
+        else {
+            $json = json_encode($exif['img\\' . $name]);
+        }
         $response = $this->response->withStringBody($json);
-        return $response; 
+        return $response;
     }
 }
