b96afb35e776310b784f0ddf9108f03dd5d0252d
Louis Charnay <louis.charnay@orange.fr>
2022-01-30 14:59:46 +0100

    Tp1ex5

--- src/Controller/PicturesController.php
+++ src/Controller/PicturesController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
-class PicturesController extends AppController {
+use Cake\Http\Exception\BadRequestException;
+
+class PicturesController extends AppController { 
     public function index() {
         $pictures = glob('img/*.jpg');
         $json = json_encode($pictures);
@@ -38,5 +40,25 @@ class PicturesController extends AppController {
         $json = json_encode($exif);
         $response = $this->response->withStringBody($json);
         return $response;
+    }
+    
+    public function select($name) {
+        $pictures = glob('img\\'. $name . '.jpg');
+        if ($pictures == null){
+            throw new BadRequestException;
+        }
+        else {
+            foreach($pictures as $image) {
+                $exif[$image]['description'] = exif_read_data($image)['ImageDescription']??'No descritpion';
+                $exif[$image]['comment'] = exif_read_data($image)['COMPUTED']['UserComment']??'No Comment';
+                $exif[$image]['author'] = exif_read_data($image)['Artist']??'No author';
+                $exif[$image]['width'] = exif_read_data($image)['COMPUTED']['Width']??'No width';
+                $exif[$image]['height'] = exif_read_data($image)['COMPUTED']['Height']??'No height';
+                $exif[$image]['html'] = '<img src=\\..\\' . $image . ' alt=' . $exif[$image]['comment'] . '>';
+            }
+            $json = json_encode($exif);
+            $response = $this->response->withStringBody($json);
+            return $response;
+        }
     }
 }
