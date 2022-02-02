a79018fd2dfe0d15179c0ca6eaabface0a01c164
Pablo Guinard <93989499+PabloGuinard@users.noreply.github.com>
2022-02-02 11:05:21 +0100

    ajout images

--- templates/element/banner.php
+++ templates/element/banner.php
@@ -1,29 +1,29 @@
-<?php include_once __DIR__.'/../class/db.php';
-$db1 = new db();
+<?//php include_once __DIR__.'/../class/db.php';
+//$db1 = new db();
 ?>
 
 <div id="header">
     <p id="titrePablo">
-        <?php 
+        <?php
             if (isset($_SESSION['id'])){
-                echo $db1->getNom((string) $_SESSION['id']);
+                //echo $db1->getNom((string) $_SESSION['id']);
             }
             else {
                 echo "Bienvenue";
             }
-            
+
         ?>
     </p>
     <img src="/img/logo_html.png" alt="logo HTML" id="logo">
     <p id="titreGuinard">
-        <?php 
+        <?php
             if (isset($_SESSION['id'])){
-                echo $db1->getPrenom((string) $_SESSION['id']);
+                //echo $db1->getPrenom((string) $_SESSION['id']);
             }
             else {
                 echo "Invité";
             }
-            
+
         ?>
     </p>
     <h1 id="hidden">ACCUEIL</h1>
