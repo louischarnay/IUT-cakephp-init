e6d9310e49ba9c8b4cdc1df8f61733a0a8df6def
Louis Charnay <louis.charnay@orange.fr>
2022-02-08 09:45:43 +0100

    Tp3ex4

--- src/Controller/PicturesController.php
+++ src/Controller/PicturesController.php
@@ -37,13 +37,13 @@ class PicturesController extends AppController {
         if ($name == -1) {
             $pictures = $this->Pictures
                 ->find()
-                ->all()
+                ->contain('Comments')
                 ->toArray();
         }
         else {
             $pictures = $this->Pictures
                 ->find()
-                ->select(['name', 'path'])
+                ->contain('Comments')
                 ->where(['name LIKE' => $name])
                 ->toArray();
         }
@@ -60,6 +60,11 @@ class PicturesController extends AppController {
                 $exif[$image->name]['width'] = exif_read_data($image->path)['COMPUTED']['Width']??'No width';
                 $exif[$image->name]['height'] = exif_read_data($image->path)['COMPUTED']['Height']??'No height';
                 $exif[$image->name]['html'] = '<img src=..\\' . $image->path . ' alt=' . $exif[$image->name]['comment'] . '>';
+                $cpt2 = 0;
+                foreach($image['comments'] as $comment){
+                    $exif[$image->name]['comments'][$cpt2] = $comment->content;
+                    $cpt2++;
+                }
                 $cpt++;
             }
         }
@@ -71,16 +76,14 @@ class PicturesController extends AppController {
     public function select($name) {
         $pictures = $this->Pictures
             ->find()
-            ->select(['name', 'path'])
-            ->where(['name LIKE' => $name])
             ->contain('Comments')
+            ->where(['name LIKE' => $name])
             ->toArray();
         if ($pictures == null){
             throw new BadRequestException;
         }
         else {
             foreach($pictures as $image) {
-                dd($image);
                 $exif[$image->name]['name'] = exif_read_data($image->path)['FileName']??'No name';
                 $exif[$image->name]['description'] = exif_read_data($image->path)['ImageDescription']??'No descritpion';
                 $exif[$image->name]['comment'] = exif_read_data($image->path)['COMPUTED']['UserComment']??'No Comment';
@@ -88,8 +91,14 @@ class PicturesController extends AppController {
                 $exif[$image->name]['width'] = exif_read_data($image->path)['COMPUTED']['Width']??'No width';
                 $exif[$image->name]['height'] = exif_read_data($image->path)['COMPUTED']['Height']??'No height';
                 $exif[$image->name]['html'] = '<img src=\\..\\' . $image->path . ' alt=' . $exif[$image->name]['comment'] . '>';
+                $cpt = 0;
+                foreach($image['comments'] as $comment){
+                    $comments[$image->name][$cpt] = $comment->content;
+                }
+                //dd($comments[$image->name][0]);
                 $this->set(compact('image'));
                 $this->set(compact('exif'));
+                $this->set(compact('comments'));
             }
         }
     }
--- templates/Pictures/select.php
+++ templates/Pictures/select.php
@@ -1,5 +1,5 @@
 <?php $this->assign('title', $exif[$image->name]['name']) ?>
-<h1><?= $exif[$image->name]['name'] ?></h1>
+<h2><?= $exif[$image->name]['name'] ?></h2>
 <?= $exif[$image->name]['html'] ?>
 <p>Description : <?= $exif[$image->name]['description'] ?></p>
 <p>Comment : <?= $exif[$image->name]['comment'] ?></p>
@@ -8,3 +8,9 @@
 <p>Height : <?= $exif[$image->name]['height'] ?></p>
 <?php $root = 'http://projetcakephp.test/img/imgAPI/' . $exif[$image->name]['name'] ?>
 <a href=<?=$root?> download=<?= $exif[$image->name]['name'] ?>>Télécharger l'image</a>
+<h2>Commentaires :</h2>
+<?php
+    $cpt = 0;
+    foreach ($comments[$image->name] as $comment){
+        echo '<p>N°' . $cpt . ' : ' . $comment;
+    }
